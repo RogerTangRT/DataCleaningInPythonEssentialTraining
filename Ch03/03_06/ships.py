91918bede5fab1f8502074bcbc4aa18ca78636b3
Roger Tang <rogertangrt@hotmail.com>
2026-07-07 11:32:39 -0300

    Mais checagem com copilor

--- Ch03/03_06/ships.py
+++ Ch03/03_06/ships.py
@@ -7,10 +7,31 @@ df
 import pandera as pa
 import numpy as np
 
-schema = pa.DataFrameSchema({
-    'name': pa.Column(pa.String),
-    'lat': pa.Column(pa.Float),
-    'lng': pa.Column(pa.Float),
-})
+
+schema = pa.DataFrameSchema(
+    {
+        "name": pa.Column(
+            pa.String,
+            unique=True,
+            checks=pa.Check.str_length(min_value=2)
+        ),
+
+        "lat": pa.Column(
+            pa.Float,
+            nullable=True,
+            checks=pa.Check.in_range(-90, 90)
+        ),
+
+        "lng": pa.Column(
+            pa.Float,
+            nullable=True,
+            checks=pa.Check.in_range(-180, 180)
+        ),
+    },
+    strict=True,
+)
+
 
 schema.validate(df)
+
+# %%
