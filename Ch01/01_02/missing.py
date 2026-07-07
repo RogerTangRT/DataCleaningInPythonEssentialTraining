07c21c87cac21223c07d049bb9de824e6a9b67fc
Roger Tang <rogertangrt@hotmail.com>
2026-07-07 08:39:32 -0300

    Missing Values

--- Ch01/01_02/missing.py
+++ Ch01/01_02/missing.py
@@ -2,8 +2,12 @@
 import pandas as pd
 
 # %%
+# NaN - Not a Number
+# NaT - Not a Time
 df = pd.read_csv('cart.csv', parse_dates=['date'])
 df
+# %%
+print("DataType\n",df.dtypes)
 
 # %%
 df['amount'].astype('Int32')
@@ -12,4 +16,6 @@ df['amount'].astype('Int32')
 df.isnull()
 
 # %%
+# Linhas que possuem valores faltantes
 df.isnull().any(axis=1)
+# %%
