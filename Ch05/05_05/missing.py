d3c138e7dd543f2bc156eec9cc69da19753ee124
Roger Tang <rogertangrt@hotmail.com>
2026-07-08 10:24:36 -0300

    Filling missing values

--- Ch05/05_05/missing.py
+++ Ch05/05_05/missing.py
@@ -6,16 +6,20 @@ df = pd.read_csv('cart.csv', parse_dates=['date'])
 df
 
 # %% 
-df['amount'].fillna(1, inplace=True)
+# df['amount'].fillna(1, inplace=True)
+df.fillna({'amount': 1}, inplace=True)
 df
 
 # %%
 most_common = df['name'].mode()[0]
-df['name'].fillna(most_common, inplace=True)
+# df['name'].fillna(most_common, inplace=True)
+df['name'] = df['name'].fillna(most_common)
+
 df
 
 # %%
-df['date'].fillna(method='ffill', inplace=True)
+# df['date'].fillna(method='ffill', inplace=True)
+df['date'] = df['date'].ffill()
 df
 
 # %%
@@ -24,5 +28,7 @@ prices = df.groupby('name')['price'].transform(np.mean)
 prices
 
 # %%
-df['price'].fillna(prices, inplace=True)
+# df['price'].fillna(prices, inplace=True)
+df['price'] = df['price'].fillna(prices)
 df
+# %%
